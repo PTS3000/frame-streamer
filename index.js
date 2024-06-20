6350c105f4376b4779482ab58c31efe44f7814cb
luisparrajunitec <luisjgcsp@gmail.com>
2024-06-20 17:28:30 +0100

    new build

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const capture = async (page) => {
 const main = async () => {
   console.log('Starting browser...');
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox', `--headless`,],
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
   process.on('exit', () => {
--- install_puppeteer.js
+++
@@ -1,7 +0,0 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browserFetcher = puppeteer.createBrowserFetcher();
-  const revisionInfo = await browserFetcher.download('1095492');
-  console.log(`Chromium downloaded and installed to ${revisionInfo.folderPath}`);
-})();
