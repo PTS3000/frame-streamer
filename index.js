524128f4a82055ec3ae5bc65f23fae0ea6b5a6d9
luisparrajunitec <luisjgcsp@gmail.com>
2024-06-23 15:09:58 +0100

    mjpeg endpoint created

--- index.js
+++ index.js
@@ -39,27 +39,54 @@ const main = async () => {
 main();
 
 const sendScreenshotFrame = (req, res) => {
+  const timestamp = Date.now();
   if (latestScreenshotBuffer) {
     res.set('Content-Type', 'text/html');
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    res.set('Surrogate-Control', 'no-store');
     res.send(`
         <!DOCTYPE html>
         <html>
         <head>
             <title>Latest frame</title>
-            <meta
-            property="og:image"
-            content="https://frame-manada-trial-20.localcan.dev/api/single-screenshot"
-            />
+            <meta property="og:image" content="https://frame-manada-trial-20.localcan.dev/api/mjpeg-stream" />
             <meta property="fc:frame" content="vNext" />
-            <meta
-            property="fc:frame:image"
-            content="https://frame-manada-trial-20.localcan.dev/api/single-screenshot"
-            />
+            <meta property="fc:frame:image" content="https://frame-manada-trial-20.localcan.dev/api/mjpeg-stream" />
             <meta property="fc:frame:button:1" content="Donate" />
-            <meta
-            property="fc:frame:post_url"
-            content="https://frame-manada-trial-20.localcan.dev/api/next-frame"
-            />
+            <meta property="fc:frame:button:1:action" content="post" />
+            <meta property="fc:frame:button:1:target" content="https://frame-manada-trial-20.localcan.dev/api/next-frame" />
+        </head>
+        <body>
+            <h1>Basic Frame</h1>
+        </body>
+        </html>
+    `);
+  } else {
+    res.status(404).send('No screenshot available');
+  }
+};
+
+const handlePostRequest = (req, res) => {
+  const timestamp = Date.now();
+  if (latestScreenshotBuffer) {
+    res.set('Content-Type', 'text/html');
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    res.set('Surrogate-Control', 'no-store');
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Latest frame</title>
+            <meta property="og:image" content="https://frame-manada-trial-20.localcan.dev/api/mjpeg-stream" />
+            <meta property="fc:frame" content="vNext" />
+            <meta property="fc:frame:image" content="https://frame-manada-trial-20.localcan.dev/api/mjpeg-stream" />
+            <meta property="fc:frame:button:1" content="Donate" />
+            <meta property="fc:frame:button:1:action" content="post" />
+            <meta property="fc:frame:button:1:target" content="https://frame-manada-trial-20.localcan.dev/api/next-frame" />
         </head>
         <body>
             <h1>Basic Frame</h1>
@@ -74,6 +101,10 @@ const sendScreenshotFrame = (req, res) => {
 const sendLatestScreenshot = (req, res) => {
   if (latestScreenshotBuffer) {
     res.set('Content-Type', 'image/png');
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    res.set('Surrogate-Control', 'no-store');
     res.send(latestScreenshotBuffer);
   } else {
     res.status(404).send('No screenshot available');
@@ -81,11 +112,12 @@ const sendLatestScreenshot = (req, res) => {
 };
 
 app.get('/api/next-frame', sendScreenshotFrame);
+app.post('/api/next-frame', handlePostRequest);
 app.get('/api/single-screenshot', sendLatestScreenshot);
 
 app.get('/api/stream', (req, res) => {
   res.writeHead(200, {
-    'Cache-Control': 'no-cache',
+    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
     'Pragma': 'no-cache',
     'Content-Type': 'multipart/x-mixed-replace; boundary=frame',
   });
@@ -104,6 +136,56 @@ app.get('/api/stream', (req, res) => {
     clearInterval(intervalId);
   });
 });
+
+const clients = [];
+
+app.get('/api/mjpeg-stream', (req, res) => {
+  const headers = {
+    'Cache-Control': 'private, no-cache, no-store, max-age=0',
+    'Content-Type': 'multipart/x-mixed-replace; boundary=mjpeg',
+    'Connection': 'close',
+    'Pragma': 'no-cache',
+  };
+
+  res.writeHead(200, headers);
+
+  const client = {
+    mjpegwrite: (buffer) => {
+      res.write('--mjpeg\r\n', 'ascii');
+      res.write('Content-Type: image/jpeg\r\n');
+      res.write('Content-Length: ' + buffer.length + '\r\n');
+      res.write('\r\n', 'ascii');
+      res.write(buffer, 'binary');
+      res.write('\r\n', 'ascii');
+    },
+    mjpegend: () => {
+      res.end();
+    },
+  };
+
+  const close = () => {
+    const index = clients.indexOf(client);
+    if (index !== -1) {
+      clients.splice(index, 1);
+    }
+  };
+
+  res.on('finish', close);
+  res.on('close', close);
+  res.on('error', close);
+
+  clients.push(client);
+});
+
+const mjpegsend = (buffer) => {
+  clients.forEach((client) => client.mjpegwrite(buffer));
+};
+
+setInterval(() => {
+  if (latestScreenshotBuffer) {
+    mjpegsend(latestScreenshotBuffer);
+  }
+}, 1000);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
