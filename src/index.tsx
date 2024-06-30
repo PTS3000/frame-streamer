36aa82da44f8d8e850b2d892e8645f2f9d8622ac
luisparrajunitec <luisjgcsp@gmail.com>
2024-06-30 11:15:40 +0200

    working

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import sharp from "sharp";
 
 const app = new Hono();
 const port = Number.parseInt(process.env.PORT ?? "8080");
-const baseUrl = "https://framer-54.localcan.dev";
+const baseUrl = "https://manadaframe-13.localcan.dev";
 const streamUrl = `${baseUrl}/api/stream`;
 const latestUrl = `${baseUrl}/api/latest`;
 const waitDelay = 1000;
